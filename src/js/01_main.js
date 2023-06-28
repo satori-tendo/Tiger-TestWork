526aba851c84b1074b10e52b08feed060eb0ea38
Edward <satori.tendo.00@mail.ru>
2023-06-28 19:55:14 +0300

    changes

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -41,6 +41,8 @@ anychart.onDocumentReady(function () { // FUNNEL CHART
     chart.draw();
 });
 
+
+
 document.getElementById('showMyDeals').onclick = function () { // обработчик на кнопку чтобы показывался сайдбар "Мои дела"
     document.getElementById('my-work').style.display = 'block'
     document.getElementById('showMyDeals').style.backgroundColor = '#273142'
@@ -56,10 +58,79 @@ document.getElementById('my-work__cross-button').onclick = function () { // об
 
 
 document.getElementById('button-more-menu').onclick = function () { // обработчик на кнопку чтобы показывался сайдбар "Мои дела"
-    let moreMenu = document.getElementById('menu-more') 
-    if(moreMenu.style.display == 'none') {
+    let moreMenu = document.getElementById('menu-more')
+    if (moreMenu.style.display == 'none') {
         moreMenu.style.display = 'block'
     } else {
         moreMenu.style.display = 'none'
+    }
+};
+
+
+
+
+document.getElementById('button-question').onclick = function () {  // HELP-USER
+    let helpUserPopUp = document.getElementById('help-user')
+    helpUserPopUp.style.display = 'block'
+};
+document.getElementById('button-close-help-user').onclick = function () {  // HELP-USER
+    let helpUserPopUp = document.getElementById('help-user')
+    helpUserPopUp.style.display = 'none'
+};
+
+
+
+
+
+
+document.getElementById('widget-dots-button').onclick = function () { // обработчик на кнопку чтобы показывался сайдбар "Мои дела"
+    let widgetSettings = document.getElementById('widget-settings')
+    if (widgetSettings.style.display == 'none') {
+        widgetSettings.style.display = 'block'
+    } else {
+        widgetSettings.style.display = 'none'
+    }
+};
+
+
+
+
+document.getElementById('delete-widget-button').onclick = function () {  // HELP-USER
+    let popUpDeleteWidget = document.getElementById('delete-pop-up')
+    popUpDeleteWidget.style.display = 'block'
+};
+document.getElementById('cancel-delete-button').onclick = function () {  // HELP-USER
+    let popUpDeleteWidget = document.getElementById('delete-pop-up')
+    popUpDeleteWidget.style.display = 'none'
+};
+
+
+
+document.getElementById('add-task-button').onclick = function () { // ADD-TASK
+    let addTaskMenu = document.getElementById('add-task')
+    if (addTaskMenu.style.display == 'none') {
+        addTaskMenu.style.display = 'block'
+    } else {
+        addTaskMenu.style.display = 'none'
+    }
+};
+
+
+
+document.getElementById('dots-button').onclick = function () { // ADD-TASK
+    let dotsMenu = document.getElementById('dots-settings')
+    if (dotsMenu.style.display == 'none') {
+        dotsMenu.style.display = 'block'
+    } else {
+        dotsMenu.style.display = 'none'
+    }
+};
+
+document.getElementById('interval-button').onclick = function () { // ADD-TASK
+    let intervalMenu = document.getElementById('interval-menu')
+    if (intervalMenu.style.display == 'none') {
+        intervalMenu.style.display = 'block'
+    } else {
+        intervalMenu.style.display = 'none'
     }
 };
