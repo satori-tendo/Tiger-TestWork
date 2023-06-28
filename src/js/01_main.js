76cacdc64d8e0ea863d7d86a385a30e0742627bf
Edward <satori.tendo.00@mail.ru>
2023-06-28 16:56:29 +0300

    global-menu, more-menu, my-work

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,61 +1,65 @@
-anychart.onDocumentReady(function () {
-    // create pie chart with passed data
+anychart.onDocumentReady(function () { // DONUT CHART
     var chart = anychart.pie([
-      ['Александра', 5.5],
-      ['Владимир', 0.5],
-      ['Тимур', 1],
-      ['Ангелина Сейт', 2],
-      ['Денис', 1]
+        ['Александра', 5.5],
+        ['Владимир', 0.5],
+        ['Тимур', 1],
+        ['Ангелина Сейт', 2],
+        ['Денис', 1]
     ]);
-
-    // set chart title text settings
     chart
-      .title('')
-      // create empty area in pie chart
-      .innerRadius('50%');
-
-    // set chart labels position to outside
+        .title('')
+        .innerRadius('50%');
     chart.labels().position('outside');
 
-    // set container id for the chart
     chart.container('chart__donut');
-    // initiate chart drawing
     chart.draw();
 });
 
 
 
-anychart.onDocumentReady(function () {
-  // prepare data for the chart
-  var data = [
-    ['Новый лид', 1],
-    ['Взяли в работу', 2],
-    ['Квалифицирован', 1],
-    ['Бриф отправлен', 0],
-    ['Бриф согласован', 1]
-    ['Договор/счет выставлен', 1]
-    ['Предоплата получена', 1]
-  ];
+anychart.onDocumentReady(function () { // FUNNEL CHART
+    var data = [
+        ['Новый лид', 1],
+        ['Взяли в работу', 2],
+        ['Квалифицирован', 1],
+        ['Бриф отправлен', 0],
+        ['Бриф согласован', 1]
+        ['Договор/счет выставлен', 1]
+        ['Предоплата получена', 1]
+    ];
 
-  // create funnel chart
-  var chart = anychart.funnel(data);
+    var chart = anychart.funnel(data);
 
-  // set chart margin
-  chart
-    .margin(10, '20%', 10, '20%')
-    // set chart base width settings
-    .baseWidth('70%')
-    // set the neck height
-    // .neckHeight('17%');
+    chart
+        .margin(10, '20%', 10, '20%')
+        .baseWidth('70%')
 
-  // set chart labels settings
-  chart.labels().position('outside-left').format('{%X} - {%Value}');
+    chart.labels().position('outside-left').format('{%X} - {%Value}');
 
-  // enable animation
-  // chart.animation(true);
 
-  // set container id for the chart
-  chart.container('chart__funnel');
-  // initiate chart drawing
-  chart.draw();
+    chart.container('chart__funnel');
+    chart.draw();
 });
+
+document.getElementById('showMyDeals').onclick = function () { // обработчик на кнопку чтобы показывался сайдбар "Мои дела"
+    document.getElementById('my-work').style.display = 'block'
+    document.getElementById('showMyDeals').style.backgroundColor = '#273142'
+};
+document.getElementById('my-work__cross-button').onclick = function () { // обработчк чтобы сайдбар "Мои дела" закрывался нажав на крестик
+    let myDeals = document.getElementById('my-work')
+    let showMyDealsButton = document.getElementById('showMyDeals')
+    myDeals.style.display = 'none'
+    showMyDealsButton.style.backgroundColor = '#354052'
+};
+
+
+
+
+document.getElementById('button-more-menu').onclick = function () { // обработчик на кнопку чтобы показывался сайдбар "Мои дела"
+    let moreMenu = document.getElementById('menu-more') 
+    if(moreMenu.style.display == 'none') {
+        moreMenu.style.display = 'block'
+    } else {
+        moreMenu.style.display = 'none'
+    }
+};
